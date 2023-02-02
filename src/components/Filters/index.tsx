0c1bc7d29cec5401614fe8dc04204cfb26236723
Dmytro Iudin <dmtry.iudin@gmail.com>
2023-02-02 16:07:07 +0200

    feat: filters added

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -33,7 +33,7 @@ const Filters = () => {
           </div>
         </div>
         <div className={styles["filters-wrapper__separator"]} />
-        <div id="sort" className={styles["filters-wrapper__item"]}>
+        <div className={styles["filters-wrapper__item"]}>
           <div
             className={styles["filters-wrapper__text-wrapper"]}
             onClick={() => setIsSortOpened((prev) => !prev)}
@@ -48,22 +48,216 @@ const Filters = () => {
               " " +
               (isSortOpened ? styles["filters-wrapper__sort_opened"] : "")
             }
-          >
-            <RadioButton label="button" />
-            <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
-            <Range title="geerg" />
-          </div>
+          ></div>
         </div>
       </div>
 
       <div
-        id="filter"
         className={
           styles["filters-wrapper__filter"] +
           " " +
           (isFilterOpened ? styles["filters-wrapper__filter_opened"] : "")
         }
-      ></div>
+      >
+        <div className={styles["filters-wrapper__filter-content-wide"]}>
+          <div className={styles["filters-wrapper__column"]}>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                CATEGORIES
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                MATERIALS
+              </div>
+              <div className={styles["filters-wrapper__category-wrapper"]}>
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+              </div>
+            </div>
+          </div>
+          <div className={styles["filters-wrapper__column"]}>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                Probe
+              </div>
+              <div className={styles["filters-wrapper__category-wrapper"]}>
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+              </div>
+            </div>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                gender
+              </div>
+              <div className={styles["filters-wrapper__category-wrapper"]}>
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+              </div>
+            </div>
+            <div></div>
+          </div>
+          <div className={styles["filters-wrapper__column"]}>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                type of gem
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                sizes
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+          </div>
+          <div className={styles["filters-wrapper__column"]}>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                prices
+              </div>
+              <div className={styles["filters-wrapper__category-wrapper"]}>
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+              </div>
+            </div>
+            <div>
+              <Range title="Or set your custom price range" />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles["filters-wrapper__filter-content-tablet"]}>
+          <div className={styles["filters-wrapper__column"]}>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                CATEGORIES
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                type of gem
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                Probe
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                gender
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+          </div>
+          <div className={styles["filters-wrapper__column"]}>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                MATERIALS
+              </div>
+              <div className={styles["filters-wrapper__category-wrapper"]}>
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+              </div>
+            </div>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                sizes
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+          </div>
+          <div className={styles["filters-wrapper__column"]}>
+            <div>
+              <div className={styles["filters-wrapper__category-title"]}>
+                prices
+              </div>
+              <div className={styles["filters-wrapper__category-wrapper"]}>
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+                <RadioButton label="button" />
+              </div>
+            </div>
+            <div>
+              <Range title="Or set your custom price range" />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles["filters-wrapper__filter-content-mobile"]}>
+          <div className={styles["filters-wrapper__column"]}>
+            <div className={styles["filters-wrapper__row"]}>
+              <div className={styles["filters-wrapper__category-title"]}>
+                MATERIALS
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div className={styles["filters-wrapper__row"]}>
+              <div className={styles["filters-wrapper__category-title"]}>
+                MATERIALS
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div className={styles["filters-wrapper__row"]}>
+              <div className={styles["filters-wrapper__category-title"]}>
+                MATERIALS
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div className={styles["filters-wrapper__row"]}>
+              <div className={styles["filters-wrapper__category-title"]}>
+                MATERIALS
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div className={styles["filters-wrapper__row"]}>
+              <div className={styles["filters-wrapper__category-title"]}>
+                MATERIALS
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div className={styles["filters-wrapper__row"]}>
+              <div className={styles["filters-wrapper__category-title"]}>
+                MATERIALS
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div className={styles["filters-wrapper__row"]}>
+              <div className={styles["filters-wrapper__category-title"]}>
+                MATERIALS
+              </div>
+              <DropDownFilter items={["ervevr", "ff34f3", "qwwqdqdwqw"]} />
+            </div>
+            <div className={styles["filters-wrapper__row"]}>
+              <Range title="Or set your custom price range" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
