c4a45f6cbdab0eee2a057615d255aa014be3fa1f
Dmytro Iudin <dmtry.iudin@gmail.com>
2023-02-10 16:28:45 +0200

    feat: dropdown opened on click

--- src/components/DropDownFilter/index.tsx
+++ src/components/DropDownFilter/index.tsx
@@ -1,37 +1,55 @@
 import { getClassByCondition } from "@/utils";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import RadioButton from "../RadioButton";
 import styles from "./DropDownFilter.module.scss";
 import { IDropDownProps } from "./types";
 
 const DropDownFilter: React.FC<IDropDownProps> = ({ items }) => {
-  const [selectedValue, setSelectedValue] = useState<number>(-1);
+  const dropDown = useRef<HTMLDivElement | null>(null);
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+
+  const closeDropDown = (e: Event) => {
+    if (!dropDown.current?.contains(e.target as Document)) {
+      setIsOpened(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", closeDropDown);
+
+    return () => {
+      document.removeEventListener("click", closeDropDown);
+    };
+  }, []);
+
   return (
-    <div className={styles["drop-down"]}>
-      <div className={styles["drop-down__to-choose"]}>
+    <div className={styles["drop-down"]} ref={dropDown}>
+      <div
+        className={styles["drop-down__to-choose"]}
+        onClick={() => setIsOpened((prev) => !prev)}
+      >
         <span
           className={[
             styles["drop-down__selected-item"],
-            getClassByCondition(
-              selectedValue < 0,
-              styles["drop-down__selected-item_empty"]
-            ),
+            styles["drop-down__selected-item_empty"],
           ].join(" ")}
         >
-          {selectedValue < 0 ? "To choose" : items[selectedValue]}
+          {"To choose"}
         </span>
         <img
           src={require("../../assets/arrow-drop-down.png").default.src}
           className={styles["drop-down__icon"]}
         />
       </div>
-      <div className={styles["drop-down__items"]}>
-        <div className={styles["drop-down__items-wrapper"]}>
-          {items.map((e) => {
-            return <RadioButton label={e} />;
-          })}
+      {isOpened ? (
+        <div className={styles["drop-down__items"]}>
+          <div className={styles["drop-down__items-wrapper"]}>
+            {items.map((e) => {
+              return <RadioButton label={e} />;
+            })}
+          </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
